16408d4224881266eb5415234d5049414186c0b4
IronmanJay <1975686676@qq.com>
2022-05-24 10:30:46 +0800

    2022-05-24

---
+++ LeetCode/Array/Array/p66_PlusOne.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int* plusOne(int* digits, int digitsSize, int* returnSize)
+{
+	for (int i = digitsSize - 1; i >= 0; i--)
+	{
+		if (digits[i] == 9)
+		{
+			digits[i] = 0;
+		}
+		else
+		{
+			digits[i]++;
+			*returnSize = digitsSize;
+			return digits;
+		}
+	}
+	int* res = (int*)calloc((digitsSize + 1), sizeof(int));
+	res[0] = 1;
+	*returnSize = digitsSize + 1;
+	return res;
+}
+
+/*Ö÷º¯ÊýÊ¡ÂÔ*/
