6c3f3f075c7a5e7784be390c1e2170eec3d1a5a5
IronmanJay <1975686676@qq.com>
2022-05-25 10:00:19 +0800

    2022-05-25

---
+++ LeetCode/Array/Array/p35_SearchCaretPosition.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int searchInsert(int* nums, int numsSize, int target)
+{
+	int left = 0, right = numsSize - 1;
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (nums[mid] == target)
+		{
+			return mid;
+		}
+		else if (nums[mid] < target)
+		{
+			left = mid + 1;
+		}
+		else if (nums[mid] > target)
+		{
+			right = mid - 1;
+		}
+	}
+	return left;
+}
+
+/*Ö÷º¯ÊıÊ¡ÂÔ*/
