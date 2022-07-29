34a19307c65c42a00feee1d2c1c1357dd9fa47d1
IronmanJay <1975686676@qq.com>
2022-07-29 18:01:24 +0800

    2022-07-29

---
+++ LeetCode/Tree/Tree/p230_TheKthSmallestElementInABinarySearchTree.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+struct TreeNode
+{
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+};
+
+int size;
+
+void inOrder(struct TreeNode* root, int *nums)
+{
+	if (root != NULL)
+	{
+		inOrder(root->left, nums);
+		nums[size++] = root->val;
+		inOrder(root->right, nums);
+	}
+}
+
+int kthSmallest(struct TreeNode* root, int k)
+{
+	size = 0;
+	int nums[10001] = { 0 };
+	inOrder(root, nums);
+	return nums[k - 1];
+}
+
+/*Ö÷º¯ÊýÊ¡ÂÔ*/
