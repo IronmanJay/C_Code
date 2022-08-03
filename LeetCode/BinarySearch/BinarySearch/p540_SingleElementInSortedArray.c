dac1072cda16c6653de3c4810aa5a586352105a6
IronmanJay <1975686676@qq.com>
2022-08-03 17:37:17 +0800

    2022-08-03

---
+++ LeetCode/BinarySearch/BinarySearch/p540_SingleElementInSortedArray.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int singleNonDuplicate(int* nums, int numsSize)
+{
+	int left = 0;
+	int right = numsSize - 1;
+	while (left < right)
+	{
+		int mid = left + (right - left) / 2;
+		if (mid % 2 == 1)
+		{
+			mid--;
+		}
+		if (nums[mid] == nums[mid + 1])
+		{
+			left = mid + 2;
+		}
+		else
+		{
+			right = mid;
+		}
+	}
+	return nums[left];
+}
+
+/*Ö÷º¯ÊýÊ¡ÂÔ*/
