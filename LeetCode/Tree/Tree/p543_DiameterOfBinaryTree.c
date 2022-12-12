9d70a26e082668c8a4e631fa0c411dfe9a2e37ab
IronmanJay <1975686676@qq.com>
2022-12-12 11:01:57 +0800

    2022-12-12

---
+++ LeetCode/Tree/Tree/p543_DiameterOfBinaryTree.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+struct TreeNode
+{
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+};
+
+int dfsDiameter(struct TreeNode* root, int* res)
+{
+	if (root == NULL)
+	{
+		return 0;
+	}
+	int left = dfsDiameter(root->left, res);
+	int right = dfsDiameter(root->right, res);
+	*res = fmax(*res, left + right);
+	return fmax(left, right) + 1;
+}
+
+int diameterOfBinaryTree(struct TreeNode* root)
+{
+	int res = 0;
+	dfsDiameter(root, &res);
+	return res;
+}
+
+/*Ö÷º¯ÊýÊ¡ÂÔ*/
