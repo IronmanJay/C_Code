c27d8b43e520be1b5721fa64cb6eb74c3071c14a
IronmanJay <1975686676@qq.com>
2022-12-21 10:19:23 +0800

    2022-12-21

---
+++ LeetCode/DynamicProgramming/DynamicProgramming/I0801_ThreeStepsProblemLcci.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int waysToStep(int n)
+{
+	if (n == 1)
+	{
+		return 1;
+	}
+	if (n == 2)
+	{
+		return 2;
+	}
+	if (n == 3)
+	{
+		return 4;
+	}
+	int* dp = (int*)malloc(sizeof(int) * (n + 1));
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 4;
+	for (int i = 4; i <= n; i++)
+	{
+		dp[i] = ((dp[i - 1] + dp[i - 2]) % 1000000007 + dp[i - 3]) % 1000000007;
+	}
+	return dp[n];
+}
+
+/*Ö÷º¯ÊýÊ¡ÂÔ*/
