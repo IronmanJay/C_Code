7ad5934990df5b721d9497f14e62cded353fb5d1
IronmanJay <1975686676@qq.com>
2022-12-31 01:20:37 +0800

    2022-12-31

---
+++ LeetCode/BinarySearch/BinarySearch/p154_FindMinimumInRotatedSortedArrayII .c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int findMin_p154_FindMinimumInRotatedSortedArrayII(int* nums, int numsSize)
+{
+	int left = 0;
+	int right = numsSize - 1;
+	while (left < right)
+	{
+		int mid = (left + right) / 2;
+		if (nums[mid] < nums[right])
+		{
+			right = mid;
+		}
+		else if (nums[mid] > nums[right])
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			right--;
+		}
+	}
+	return nums[left];
+}
+
+/*Ö÷º¯ÊýÊ¡ÂÔ*/
