86315c46805baa2dbbe8349936981448f0dd50ef
IronmanJay <1975686676@qq.com>
2023-01-03 11:13:47 +0800

    2023-01-03

---
+++ LeetCode/Math/Math/p507_PerfectNumber.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool checkPerfectNumber(int num)
+{
+	if (num == 1)
+	{
+		return false;
+	}
+	int res = 1;
+	for (int i = 2; i * i <= num; i++)
+	{
+		if (num % i == 0)
+		{
+			res += i;
+			if (i * i < num)
+			{
+				res += num / i;
+			}
+		}
+	}
+	return res == num;
+}
+
+/*Ö÷º¯ÊýÊ¡ÂÔ*/
