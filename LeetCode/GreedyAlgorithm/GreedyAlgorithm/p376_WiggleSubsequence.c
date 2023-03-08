81f644b5de13b023b1afca043328e6c716203edb
IronmanJay <1975686676@qq.com>
2023-03-08 10:55:37 +0800

    2023-03-08

---
+++ LeetCode/GreedyAlgorithm/GreedyAlgorithm/p376_WiggleSubsequence.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int wiggleMaxLength(int* nums, int numsSize)
+{
+	if (numsSize < 2)
+	{
+		return numsSize;
+	}
+	int preDiff = nums[1] - nums[0];
+	int res = preDiff != 0 ? 2 : 1;
+	for (int i = 2; i < numsSize; i++)
+	{
+		int diff = nums[i] - nums[i - 1];
+		if ((preDiff >= 0 && diff < 0) || (preDiff <= 0 && diff > 0))
+		{
+			res++;
+			preDiff = diff;
+		}
+	}
+	return res;
+}
+
+/*Ö÷º¯ÊýÊ¡ÂÔ*/
