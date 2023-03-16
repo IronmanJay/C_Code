dac72f50ddcdbe41d887f558749f1c63ed1931fc
IronmanJay <1975686676@qq.com>
2023-03-16 11:09:24 +0800

    2023-03-16

---
+++ LeetCode/DoublePointer/DoublePointer/p283_MoveZeroes.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void moveZeroes(int* nums, int numsSize)
+{
+	for (int i = 0, j = 0; i < numsSize; i++)
+	{
+		if (nums[i] != 0)
+		{
+			int temp = nums[i];
+			nums[i] = nums[j];
+			nums[j++] = temp;
+		}
+	}
+}
+
+/*Ö÷º¯ÊýÊ¡ÂÔ*/
