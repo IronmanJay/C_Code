f7dbf3d570bda10a51175ebbd1021793cf1ed58b
IronmanJay <1975686676@qq.com>
2023-03-19 09:50:45 +0800

    2023-03-19

---
+++ LeetCode/DoublePointer/DoublePointer/p165_CompareVersionNumbers.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+
+int compareVersion(char * version1, char * version2)
+{
+	int len1 = strlen(version1);
+	int len2 = strlen(version2);
+	int index1 = 0;
+	int index2 = 0;
+	while (index1 < len1 || index2 < len2)
+	{
+		long x = 0;
+		while (index1 < len1 && version1[index1] != '.')
+		{
+			x += x * 10 + version1[index1] - '0';
+			index1++;
+		}
+		index1++;
+		long y = 0;
+		while (index2 < len2 && version2[index2] != '.')
+		{
+			y += y * 10 + version2[index2] - '0';
+			index2++;
+		}
+		index2++;
+		if (x != y)
+		{
+			return x > y ? 1 : -1;
+		}
+	}
+	return 0;
+}
+
+/*Ö÷º¯ÊıÊ¡ÂÔ*/
