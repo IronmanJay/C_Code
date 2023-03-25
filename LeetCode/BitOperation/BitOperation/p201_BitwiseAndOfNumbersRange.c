46bacaec10620a21f10a9b0755a0a08f6f4979a5
IronmanJay <1975686676@qq.com>
2023-03-25 09:25:39 +0800

    2023-03-25

---
+++ LeetCode/BitOperation/BitOperation/p201_BitwiseAndOfNumbersRange.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int rangeBitwiseAnd(int left, int right)
+{
+	int count = 0;
+	while (left != right)
+	{
+		left >>= 1;
+		right >>= 1;
+		count++;
+	}
+	return left << count;
+}
+
+/*Ö÷º¯ÊıÊ¡ÂÔ*/
